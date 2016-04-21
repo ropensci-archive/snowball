62def9b1eb91bd9c7047a9003d97542b3240075d
Jonathan Carroll <jono@jcarroll.com.au>
2016-04-21 16:13:06 +0930

    requires snowball_setup() aws bucket save/load

--- R/aws.R
+++ R/aws.R
@@ -13,7 +13,8 @@
 #' @examples
 #'
 #' \dontrun{
-#' startInstance()
+#' my_instance <- startInstance()
+#' instance_status(my_instance$item$instanceId[[1]])
 #' }
 startInstance <- function(ami="ami-1170382b",
                           type="t2.micro",
@@ -40,6 +41,61 @@ startInstance <- function(ami="ami-1170382b",
                      sgroup   = securityGroup,
                      userdata = userData)
 
+  message(paste0("Starting ",i$item$instanceId[[1]]," instance, id ",i$item$instanceId[[1]]))
+
   return(i)
+
+}
+
+
+#' List contents of a S3 bucket
+#'
+#' @param bucket
+#'
+#' @return vector of data.frame contents
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' my_bucket <- get_bucket("bucketName")
+#' bucket_contents(my_bucket)
+#' }
+bucket_contents <- function(bucket) {
+
+  lapply(my_bucket, function(x) x[["Key"]]) %>% unlist %>% unname
+
+}
+
+
+#' Perform snowball setup
+#'
+#' @param config_file location of the configuration file, default ~/.snowball
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' snowball_setup()
+#' }
+snowball_setup <- function(config_file="~/.snowball") {
+
+  # Sys.setenv("AWS_ACCESS_KEY_ID" = "AKIAIVY2VRH3SKPPY6NQ",
+  #            "AWS_SECRET_ACCESS_KEY" = "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",
+  #            "AWS_DEFAULT_REGION" = "ap-southeast-2")
+
+  if(file.exists(config_file)) {
+    config <- read.dcf(config_file, fields=c("AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_DEFAULT_REGION"))
+    Sys.setenv(AWS_ACCESS_KEY_ID = config[, "AWS_ACCESS_KEY_ID"])
+    Sys.setenv(AWS_SECRET_ACCESS_KEY = config[, "AWS_SECRET_ACCESS_KEY"])
+    Sys.setenv(AWS_DEFAULT_REGION = config[, "AWS_DEFAULT_REGION"])
+  } else {
+    warning("PLEASE ADD A ~/.snowball FILE WITH CONFIGURATION\n
+            AWS_ACCESS_KEY_ID: <YOURACCESSSKEYID>\n
+            AWS_SECRET_ACCESS_KEY: <YOURSECRETACCESSKEY>\n
+            AWS_DEFAULT_REGION: <YOURDEFAULTREGION>")
+  }
+
+  return(NULL)
 
 }
---
+++ examples/TestAWSS3Buckets.R
@@ -0,0 +1,64 @@
+#INSTALL CLOUDYR
+
+#if(!require("drat")) install.packages("drat")
+#drat::addRepo("cloudyr")
+#install.packages("aws.s3")
+
+# library("aws.s3")
+
+#
+# AWS Access Key ID [None]: AKIAIVY2VRH3SKPPY6NQ
+# AWS Secret Access Key [None]: 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
+# Default region name [None]: ap-southeast-2
+#
+# bucketlist()
+# #Generate data to test push to and from S3
+# nRows<-100;
+# nCols<-10
+# x<-matrix(rnorm(nRows*nCols),nrow=nRows,ncol=nCols)
+# write.csv(x,'testX.csv')
+#
+# #Find data on S3
+# bucketlist()
+#
+# #trying to get data to S3
+# # s3save(x, bucket = "mjktestbucketmsf", object = "x.Rdata")
+
+## THIS WORKS
+
+my_bucket <- get_bucket("jcarroll1")
+
+s3save(x, object="x", bucket=my_bucket )
+
+y <- get_object("x", bucket=my_bucket)
+
+load(rawConnection(y))
+
+
+# save_object(bucket=my_bucket, object="x", file="myX.RData")
+
+# get_object('testX.csv' , bucket = "mjktestbucketmsf")
+# put_object(file='testX.csv', bucket = "mjktestbucketmsf", object = "x.Rdata")
+#
+#
+#
+# ##############EXAMPLES FROM  Cloudyr documentation
+# #https://github.com/cloudyr/aws.s3
+# # save an in-memory R object into S3
+# s3save(mtcars, bucket = "my_bucket", object = "mtcars.Rdata")
+#
+# # `load()` R objects from the file
+# s3load("mtcars.Rdata", bucket = "my_bucket")
+#
+# # get file as raw vector
+# get_object("mtcars.Rdata", bucket = "my_bucket")
+#
+# # save file locally
+# save_object("mtcars.Rdata", file = "mtcars.Rdata", bucket = "my_bucket")
+#
+# # put local file into S3
+# put_object(file = "mtcars.Rdata", object = "mtcars2.Rdata", bucket = "my_bucket")
+#
+#
+#
+#
