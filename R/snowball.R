3949ad1afce70d9194a5005ee8bbf13939aae73a
Jonathan Carroll <jono@jcarroll.com.au>
2016-04-21 09:57:19 +0930

    initial package structure

---
+++ R/snowball.R
@@ -0,0 +1,4 @@
+# devtools::install_github("cloudyr/aws.ec2")
+install.packages("aws.ec2", repos = c(getOption("repos"), "http://cloudyr.github.io/drat"))
+library(aws.ec2)
+setwd("~/Dropbox/Freelancer/auunconf2016/snowball/")
