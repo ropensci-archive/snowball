4807b90dc288e2db5f8e549f147370798bf88f87
Jonathan Carroll <jono@jcarroll.com.au>
2016-04-21 10:06:12 +0930

    trivial

--- R/snowball.R
+++ R/snowball.R
@@ -2,3 +2,5 @@
 install.packages("aws.ec2", repos = c(getOption("repos"), "http://cloudyr.github.io/drat"))
 library(aws.ec2)
 setwd("~/Dropbox/Freelancer/auunconf2016/snowball/")
+
+## THIS WILL BE SOME CODE
