a66dd36bcf2f6d61024cea27e48abf0ce7cbd160
Jonathan Carroll <jono@jcarroll.com.au>
2016-04-21 12:26:14 +0930

    aws example and description

--- R/aws.R
+++ R/aws.R
@@ -1,27 +1,46 @@
-library(devtools)
-install_github("cloudyr/aws.ec2")
-install_github("cloudyr/aws.s3")
+# library(devtools)
+# install_github("cloudyr/aws.ec2")
+# install_github("cloudyr/aws.s3")
 
-Sys.setenv("AWS_ACCESS_KEY_ID" = "AKIAJL5GW56QOCRWPXSA",
-           "AWS_SECRET_ACCESS_KEY" = "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",
-           "AWS_DEFAULT_REGION" = "ap-southeast-2")
+#' Start an AWS instance
+#'
+#' @param ami
+#' @param type
+#' @param subNet
+#' @param securityGroup
+#' @param keyName
+#' @param userData
+#'
+#' @return
+#' @export
+#'
+#' @examples
+startInstance <- function(ami="ami-1170382b",
+                          type="t2.micro",
+                          subNet=-1,
+                          securityGroup=-1,
+                          keyName=-1,
+                          userData="") {
+  # library(aws.ec2)
 
-startInstance <- function(ami="ami-1170382b",type="t2.micro",subNet=-1,securityGroup=-1,keyName=-1,userData=""){
-  library(aws.ec2)
   #describe_images(ami)
+
   if (subNet==-1) {
     subNet <- describe_subnets()[[1]]
   }
+
   if (securityGroup==-1) {
     securityGroup <- describe_sgroups()[[1]]
   }
+
   if (userData==""){
     userData=""
   }
-  i <- run_instances(image = ami,
-                     type = type,
-                     subnet = subNet,
-                     sgroup = securityGroup,
+
+  i <- run_instances(image    = ami,
+                     type     = type,
+                     subnet   = subNet,
+                     sgroup   = securityGroup,
                      userdata = userData)
   return(i)
 }
---
+++ R/user_script.R
@@ -0,0 +1,8 @@
+## set the system variables (using Daniel's keys for now - Cheers!)
+## this should eventually be stored as .snowball config file
+Sys.setenv("AWS_ACCESS_KEY_ID"     = "AKIAJL5GW56QOCRWPXSA",
+           "AWS_SECRET_ACCESS_KEY" = "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",
+           "AWS_DEFAULT_REGION"    = "ap-southeast-2")
+
+library(snowball)
+
