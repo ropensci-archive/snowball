beba93ca4701c98d41e5ff1298e2cd429ab0e614
Jonathan Carroll <jono@jcarroll.com.au>
2016-04-21 12:43:19 +0930

    let's not run examples by default

--- R/example_test_iris.R
+++ R/example_test_iris.R
@@ -1,18 +1,18 @@
 ## adapted from https://cran.r-project.org/web/packages/doParallel/vignettes/gettingstartedParallel.pdf
 
-library(foreach)
-library(doParallel)
-
-cl <- makeCluster(2)
-registerDoParallel(cl)
-
-x <- iris[which(iris[,5] != "setosa"), c(1,5)]
-trials <- 10000L
-ptime <- system.time({
-  r <- foreach(icount(trials), .combine=cbind) %dopar% {
-    ind <- sample(100, 100, replace=TRUE)
-    result1 <- glm(x[ind,2] ~ x[ind,1], family=binomial(logit))
-    coefficients(result1)
-  }
-})[3]
-ptime
+# library(foreach)
+# library(doParallel)
+#
+# cl <- makeCluster(2)
+# registerDoParallel(cl)
+#
+# x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+# trials <- 10000L
+# ptime <- system.time({
+#   r <- foreach(icount(trials), .combine=cbind) %dopar% {
+#     ind <- sample(100, 100, replace=TRUE)
+#     result1 <- glm(x[ind,2] ~ x[ind,1], family=binomial(logit))
+#     coefficients(result1)
+#   }
+# })[3]
+# ptime
--- R/snowball.R
+++ R/snowball.R
@@ -1,7 +1,6 @@
 # devtools::install_github("cloudyr/aws.ec2")
-install.packages("aws.ec2", repos = c(getOption("repos"), "http://cloudyr.github.io/drat"))
-library(aws.ec2)
-setwd("~/Dropbox/Freelancer/auunconf2016/snowball/")
+# install.packages("aws.ec2", repos = c(getOption("repos"), "http://cloudyr.github.io/drat"))
+# library(aws.ec2)
 
 ## THIS WILL BE SOME CODE
 
