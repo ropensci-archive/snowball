e9fc4afa764c4815cb2718fac98f227fcd129633
Daniel Thomas <Daniel Thomas>
2016-04-21 16:04:22 +1000

    working single instance execute, requires generalisation

---
+++ R/snowball-package.R
@@ -0,0 +1,13 @@
+#' snowball: A package for creating and running a remote cluster
+#'
+#' Starts an instance as a head node, then spawns worker instances for calculations. Created at the 2016 ROpenSci #auunconf.
+#'
+#' @section cluster functions:
+#' The cluster functions ...
+#'
+#' @section interrogation functions:
+#' The interrogation functions ...
+#'
+#' @docType package
+#' @name snowball
+NULL
